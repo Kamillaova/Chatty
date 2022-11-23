3b528940a5fcde48c01e5ec13a25a9c81c8bcb2c
Kamillaova <54859825+Kamillaova@users.noreply.github.com>
2022-11-23 05:37:20 +0300

    ah

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,24 @@
 @file:Suppress("UnstableApiUsage")
 
 dependencyResolutionManagement {
-  dependencyResolutionManagement {
-    repositories {
-      mavenCentral()
-      maven("https://papermc.io/repo/repository/maven-public/") {
-        content {
-          includeGroup("net.md-5")
-          includeGroup("com.mojang")
-          includeGroupByRegex("io.papermc.*")
-          includeGroupByRegex("com.destroystokyo.*")
-        }
-        maven("https://repo.codemc.io/repository/nms/") {
-          content { includeGroupByRegex("com.destroystokyo.*") }
-        }
-        maven("https://jitpack.io") {
-          content { includeGroupByRegex("com.github.*") }
-        }
-        maven("https://repo.extendedclip.com/content/repositories/placeholderapi/") {
-          content { includeGroup("me.clip") }
-        }
+  repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/") {
+      content {
+        includeGroup("net.md-5")
+        includeGroup("com.mojang")
+        includeGroupByRegex("io.papermc.*")
+        includeGroupByRegex("com.destroystokyo.*")
       }
+    }
+    maven("https://repo.codemc.io/repository/nms/") {
+      content { includeGroupByRegex("com.destroystokyo.*") }
+    }
+    maven("https://jitpack.io") {
+      content { includeGroupByRegex("com.github.*") }
+    }
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/") {
+      content { includeGroup("me.clip") }
     }
   }
 }
